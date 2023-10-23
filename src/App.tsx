235b5d2d0c0be128f78c5a77e8930df6c794e070
Benoit PETIT <petitbenoit3@gmail.com>
2023-10-23 21:56:46 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+function App() {
+	return (
+		<div className='grid'>
+			<header>header</header>
+			<nav>nav</nav>
+			<aside>aside</aside>
+			<main>main</main>
+		</div>
+	)
+}
+
+export default App
